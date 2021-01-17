b1382ab97325c5a7ac0a63a3ae0a20399b895c39
DorBenayoun <74483949+DorBenayoun@users.noreply.github.com>
2021-01-17 17:43:04 +0200

    Delete trie.h

--- trie.h
+++
@@ -1,6 +0,0 @@
-#include "subFrequency.h"
-
-int newTrie(node **head);
-void printTrie(const node *root, char str[], int level);
-void printTrieb(const node *root, char str[], int level);
-void clearTrie(node *root);
